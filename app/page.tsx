37fa6f0ca6ebaddb742dc63f0ede392af9f5c99a
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-06-05 14:01:27 +0100

    implement instruction card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./header/page"
+import Instruction from "./instruction/page"
 
 export default function Home() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <Instruction/>
+    </div>
+
+    // <CreateUser/>
+    // <CreateNote/>
+    // <ListOfNotes/>
   )
 }
