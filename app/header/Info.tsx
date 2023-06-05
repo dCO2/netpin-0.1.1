ce9c5e311fd8688ffff6976d1700573991d31dfe
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-06-05 10:57:25 +0100

    added info and github links to header's menu

--- app/header/Info.tsx
+++ app/header/Info.tsx
@@ -4,9 +4,9 @@ export default function Info() {
   return (
     <div className="flex relative mr-auto h-10 w-10">
       <button
-        className="flex h-full w-full items-center justify-center rounded-full bg-slate-200"
+        className="flex h-full w-full items-center justify-center rounded-full"
       >
-        <InformationCircleIcon className="h-6 w-6"/>
+        <InformationCircleIcon className="h-7 w-7"/>
       </button>
       {/* <aside>
         <h2>How to use netpin</h2>
