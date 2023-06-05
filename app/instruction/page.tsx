d0bb33645ba690db56e0c635fb84ec20e1403e1c
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-06-05 14:14:05 +0100

    Updated README.md and Instruction card

--- app/instruction/page.tsx
+++ app/instruction/page.tsx
@@ -1,7 +1,7 @@
 export default function Instruction(){
   return(
     <div className="flex flex-col mx-2 mt-4 p-2 text-lg rounded-md md:mt-4 md:m-auto md:max-w-screen-md">
-      <h4><i>Hello there! Netpin is a mini learning-project built with Nextjs, TailwindCSS, Ant, & Netlify. Go ahead and select a username, create notes and save them to the cloud. Access & share your notes through a link made for you!</i></h4>
+      <h4><i>Hello there! Netpin is a mini learning-project built with Nextjs, TailwindCSS, Prisma, Ant, & Netlify. Go ahead and select a username, create notes and save them to the cloud. Access & share your notes through a link made for you!</i></h4>
     </div>
   )
 }
